ec5cb662ed70613c5a2024a323b87401996f9e45
coder2077 <abdulatif.tech@gmail.com>
2022-10-22 01:32:03 +0500

    lol

--- config/urls.py
+++ config/urls.py
@@ -4,9 +4,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from mediana.views import home
 
 
 urlpatterns = [
+    path('', home, name='index'), 
     path('admin/', include('mediana.urls')), 
     path('admin/doc/', include('django.contrib.admindocs.urls')), 
     path('admin/', admin.site.urls), 
--- mediana/views.py
+++ mediana/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest
 
-from .admin import StudentAdmin
+from datetime import datetime
+
+from .models import Student
 
 
 
@@ -9,14 +11,21 @@ def admin_dashboard(request: HttpRequest):
 	if request.user.is_superuser:
 		template_name = 'admin_dashboard.html'
 		return render(request=request, template_name=template_name)
-	
+
 	elif request.user.is_staff:
 		template_name = 'stuff_dashboard.html'
+		today = datetime.now().date()
+		all_users = Student.objects.all().count()
+		for user in Student.objects.all():	
+			print(f"{user.full_name}: {user.created_at}")
+		active_users = Student.objects.filter(blocked=False).count()
+		blocked_users = Student.objects.filter(blocked=True).count()
+		today_users = Student.objects.filter(created_at__date=today).count()
 		context = {
-			'all_users': 666, 
-			'active_users': 599, 
-			'blocked_users': 67, 
-			'today_users': 112
+			'all_users': all_users, 
+			'active_users': active_users, 
+			'blocked_users': blocked_users, 
+			'today_users': today_users
 		}
 		return render(request=request, template_name=template_name, context=context)
 
@@ -32,3 +41,7 @@ def send_sms_view(request: HttpRequest):
 	elif request.method == 'POST':
 		template_name = 'done_sms.html'
 		return render(request, template_name)
+
+
+def home(request: HttpRequest):
+	return redirect('/admin/')
